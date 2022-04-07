51552e7303787a451f51564d9963d97e612dc88e
aro4x4 <vlad2804@yahoo.com>
2022-04-07 18:48:52 +0300

    done reset button + set number of squares

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,12 +1,26 @@
 const mainDiv = document.getElementById('mainDiv');
 mainDiv.style.display = "inline-block";
-for(x=0; x<256; x++) {
+const btn = document.getElementById('button');
+btn.addEventListener("click", getSquare);
+function getSquare() {
+    mainDiv.querySelectorAll('*').forEach( n => n.remove() );
+    var a = 0;
+    a = prompt("Choose number of squares per side (max.100):");
+    if(a > 100) {prompt("Choose number of squares per side (max.100):")};
+    makeGrid(a);
+}
+function makeGrid(a) {
+    var b = a * a;
+for(x=0; x<b; x++) {
     var div = document.createElement('div');
     div.className = "divs";
-    div.style = "background-color: antiquewhite;";
     div.style.border = "1px solid black";
-    div.style.width = "calc(6.25% - 2px)";
-    div.style.height = "calc(6.25% - 2px)";
+    const c = 1 / a * 100;
+    div.style.width = `calc(${c}% - 2px)`;
+    div.style.height = `calc(${c}% - 2px)`;
     div.style.float = "left";
     document.getElementById('mainDiv').appendChild(div);
+    const divs = document.querySelectorAll('.divs');
+    divs.forEach(div => div.addEventListener('mouseover', () => div.classList.replace('divs' , 'color')));
+}
 }
